a3302a91463b0c9a8d39d51f20217bbabff8542f
Muhammad-Abu-Bakar767 <muhammad.bsse4761@student.iiu.edu.pk>
2025-06-27 20:57:52 +0500

    Add files via upload

---
+++ routes/listing.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const wrapAsync = require("../utils/wrapAsync");
+const { validateListing, isLoggedIn, isOwner } = require("../middleware.js");
+const listingController = require("../Controllers/listings");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage })
+
+const Listing = require("../models/listing.js");
+
+const router = express.Router();
+
+
+// Index and New listing route
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+// Update, Delete and Show listing route
+
+router
+  .route("/:id")
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing))
+  .get(wrapAsync(listingController.showListing));
+  
+
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm)
+);
+
+module.exports = router;
