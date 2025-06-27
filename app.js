444be1b862f1c0a9da4f6f4a96e66ffb4d461ef3
Muhammad-Abu-Bakar767 <muhammad.bsse4761@student.iiu.edu.pk>
2025-06-27 21:01:33 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,126 +1,134 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const ejs = require("ejs");
 const ejsMate = require("ejs-mate");
-
-require("dotenv").config();
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 const Listing = require("./models/listing");
+const User = require("./models/user.js");
 
+const ExpressError = require("./utils/ExpressError");
+const wrapAsync = require("./utils/wrapAsync");
 
-const PORT = process.env.PORT;
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+const port = 8080;
 
 const app = express();
+
+
+const store = MongoStore.create({
+
+  mongoUrl: process.env.ATLASDB_URL,
+
+  crypto: {
+    secret : process.env.SECRET,
+  },
+
+  touchAfter: 24 * 3600,
+})
+
+store.on("error", () => {
+
+  console.log("ERROR in MONGO SESSION STORE", err);
+})
+
+const sessionOptions = {
+  
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-app.listen(PORT, () => {
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-    console.log(`App listening on port ${PORT}`);
-})
+// Demo User
 
-// GET requests
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    username: "bob",
+    email: "bob@gmail.com",
+  });
 
-app.get("/", (req, res) => {
+  let registeredUser = await User.register(fakeUser, "hello");
 
-    res.send(`I am on root`);
-})
+  console.log(registeredUser);
 
-app.get("/listings", async (req, res) => {
+  res.send(registeredUser);
+});
 
-    let listings = await Listing.find();
+// root
 
-    res.render("listings/index.ejs", {listings});
-})
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user;
+  next();
+});
 
-app.get("/listings/new", (req, res) => {
+app.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    // let listings = await Listing.find();
 
-    res.render("listings/new.ejs");
-})
-
-app.get("/listings/:id", async (req, res) => {
-
-    let {id} = req.params;
-
-    const listing = await Listing.findById(id);
-
-    res.render("listings/show.ejs", {listing});
-})
-
-app.get("/listings/:id/edit", async (req, res) => {
-
-    let {id} = req.params;
-
-    let listing = await Listing.findById(id);
-
-    res.render("listings/edit.ejs", {listing});
-})
-
-
-// POST requests
-
-app.post("/listings", async (req, res) => {
-
-    let newListing = new Listing(req.body.listing);
-    let savedNewListing = await newListing.save();
-
-    res.redirect(`/listings/${savedNewListing._id}`);
-})
-
-
-// PUT requests
-
-app.put("/listings/:id", async (req, res) => {
-
-    let {id} = req.params;
-
-    await Listing.findByIdAndUpdate(id, {...req.body.listing}, {
-
-        runValidators: true
-    })
-
-    res.redirect(`/listings/${id}`);
-})
-
-
-// DELETE requests
-
-app.delete("/listings/:id", async (req, res) => {
-
-    let {id} = req.params;
-
-    await Listing.findByIdAndDelete(id);
+    // res.render("listings/root.ejs", { listings });
 
     res.redirect("/listings");
-})
+  })
+);
 
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
-//////////////
+// For all requests
 
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page not found"));
+});
 
+////////////// Middlewares
 
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Something went wrong" } = err;
+  res.status(status).render("listings/errors.ejs", { message });
+});
 
+// Starts server
 
-app.get("/test-listing", (req, res) => {
-
-    let sampleListing = new Listing({
-
-        title: "House",
-        description: "This is my new beautiful house",
-        price: 10000,
-        location: "Service Road North, I-11/2, Islamabad",
-        country: "Pakistan"
-    })
-
-    sampleListing.save();
-
-    res.send(`Warning: Sample data is stored DON'T REFRESH BEFORE CHANGING SAMPLE DATA`);
-})
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ middleware.js
@@ -0,0 +1,89 @@
+const Listing = require("./models/listing.js");
+const Review = require("./models/review");
+const {listingSchema, reviewSchema} = require("./schema.js");
+const ExpressError = require("./utils/ExpressError");
+
+const isLoggedIn = (req, res, next) => {
+
+    if (!req.isAuthenticated()) {    
+
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "You must be logged in to perform this action");
+        return res.redirect("/login");
+  }
+  
+  req.session.redirectUrl = req.originalUrl;
+
+  next();
+};
+
+const saveRedirectUrl = (req, res, next) => {
+
+    if(req.session.redirectUrl){
+
+        res.locals.redirectUrl = req.session.redirectUrl;        
+    }
+    next();
+}
+
+const isOwner = async (req, res, next) => {
+
+    let { id } = req.params;
+
+    let listing = await Listing.findById(id);
+    
+
+    if(!listing.owner.equals(res.locals.user._id)){
+
+      req.flash("error", "You are not the owner of this listing");
+      return res.redirect(`/listings/${id}`);
+    }
+
+    next();
+}
+
+const isReviewAuthor = async (req, res, next) => {
+
+  let { id, reviewId } = req.params;
+
+  let review = await Review.findById(reviewId);
+  
+  if(!review.author.equals(res.locals.user._id)){
+
+    req.flash("error", "You are not the owner of this review");
+    return res.redirect(`/listings/${id}`);
+  }
+
+  next();
+}
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+
+  if (error) {
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
+}; 
+
+
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+
+  if (error) {
+    throw new ExpressError(400, error);
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+
+    validateListing,
+    validateReview,
+    isLoggedIn,
+    saveRedirectUrl,
+    isOwner,
+    isReviewAuthor
+}
